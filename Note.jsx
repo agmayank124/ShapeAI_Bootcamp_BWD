a528cfcc550aab9251664420d7128bd3857e6d89
agmayank124 <68546206+agmayank124@users.noreply.github.com>
2021-06-05 18:31:37 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript And React JS Bootcamp</h1>
+      <p>
+        Thanks for this lovely Boootcamp Shaurya Sir. I have changed a bit of
+        styling and introduced neumorphism.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
